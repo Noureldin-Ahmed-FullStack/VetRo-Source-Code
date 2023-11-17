545bae17ef2c0ed9252bb9bce96f316e0ee93efb
Nour Eldin <noureldin2662002@gmail.com>
2023-11-17 23:37:02 +0200

    updated user inserted data and pet inserted data and added placeholder code for pets and users refrence link

--- src/Components/UseFirebaseAuth.jsx
+++ src/Components/UseFirebaseAuth.jsx
@@ -4,13 +4,13 @@ import { useContext, useEffect, useState } from 'react';
 import { MyContext } from './ContextProvider'
 import { auth, provider, app, db } from '../Firebase/firebase';
 import { InsertUserData } from './InsertToUsers';
-import { addDoc, collection, doc, getDocs, query, where } from 'firebase/firestore';
+import { addDoc, collection, doc, getDocs, query, setDoc, updateDoc, where } from 'firebase/firestore';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 const authentic = getAuth(app);
 
 export function UseFirebaseAuth() {
-  const [user, setUser] = useState(null);  
+  const [user, setUser] = useState(null);
   // const { profilePhotoURL, setprofilePhotoURL } = useContext(MyContext);
   // const { myAuth, setMyAuth } = useContext(MyContext);
   const { userObj, setUserObj } = useContext(MyContext);
@@ -18,30 +18,56 @@ export function UseFirebaseAuth() {
   // const petRef = collection(db, "Pets")
   // const petDocumentRef = doc(petRef, "1");
 
-  
 
-      /*save data of user in db (passant) */
-  const InsertUserData = () => {  
+
+  /*save data of user in db (passant) */
+  const InsertUserData = () => {
     console.log(userObj);
-    auth.onAuthStateChanged(async(trig) => {
-      if(trig){
-      const m = trig.email;
-      const emailExistsQuery = query(usersRef, where('email', '==', m));
-      const emailExistsResult = await getDocs(emailExistsQuery);
-      if (emailExistsResult.empty) {
-        addDoc(usersRef, {
-      uid: trig.uid,
-      userName: trig.displayName,
-      email:trig.email,
-      userPFP: trig.photoURL,
-      pets: null,
+    auth.onAuthStateChanged(async (trig) => {
+      if (trig) {
+        const m = trig.email;
+        const emailExistsQuery = query(usersRef, where('email', '==', m));
+        const emailExistsResult = await getDocs(emailExistsQuery);
+        if (emailExistsResult.empty) {
+          const userDoc = doc(usersRef, trig.uid);
+          setDoc(userDoc, {
+              uid: trig.uid,
+            userName: trig.displayName,
+            email: trig.email,
+            userPFP: trig.photoURL,
+            pets: null,
+          });
+        }
+      }
     });
   }
-}
-  });
-}
 
-
+  async function updatePetsFieldForUser( newPetsData) {
+    const usersCollection = collection(db, 'Users');
+  
+    try {
+      // Find the specific user document by userId
+      // const userQuery = query(usersCollection, where('uid', '==', userId));
+      // const userQuerySnapshot = await getDocs(userQuery);
+      const userDocRef = doc(usersCollection , userObj.uid);
+      if (userDocRef) {
+        // Get the reference to the user document
+        // const userDoc = userQuerySnapshot.docs[0].ref;
+  
+        // Update the "pets" field for the user document
+        console.log(userDocRef);
+        await updateDoc(userDocRef, {
+          pets: newPetsData,
+        });
+  
+        console.log(`"pets" field updated successfully for user ${userDocRef.id}`);
+      } else {
+        console.log(`User with ID ${userDocRef.id} not found`);
+      }
+    } catch (error) {
+      console.error('Error updating "pets" field:', error);
+    }
+  }
 
   // useEffect(() => {
 
@@ -58,7 +84,7 @@ export function UseFirebaseAuth() {
 
   //   return () => unsubscribe();
   // }, []);
-/////////////////
+  /////////////////
   const signInWithGoogle = async () => {
     const provider = new GoogleAuthProvider();
     try {
@@ -81,5 +107,5 @@ export function UseFirebaseAuth() {
     }
   };
 
-  return { user, signInWithGoogle, signOutUser };
+  return { user, signInWithGoogle, signOutUser , updatePetsFieldForUser};
 }
