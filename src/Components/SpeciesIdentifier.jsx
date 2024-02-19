fd74122a64abe187e773b8a751785e3b8eb026ef
noureldin <noureldin2662002@gmail.com>
2024-02-19 20:28:23 +0200

    SpeciesIdentifier edits

--- src/Components/SpeciesIdentifier.jsx
+++ src/Components/SpeciesIdentifier.jsx
@@ -3,15 +3,17 @@ import React, { useRef, useState } from 'react'
 import CameraComponent from './CameraComponent';
 import { storage, ref, uploadBytes, getDownloadURL } from '../Firebase/firebase';
 import firebase from 'firebase/app';
+import { toast } from 'react-toastify';
 import 'firebase/storage';
 import Loading from './Loading';
+import '../MyCss/SpeciesIdentifier.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as fa from '@fortawesome/free-solid-svg-icons'
 export default function SpeciesIdentifier() {
-    const [Result, setResult] = useState("result")
+    const [Result, setResult] = useState("Try it!")
     const [image, setImage] = useState(null);
     const [pending, setPending] = useState(false);
-    const [imageUrl, setImageUrl] = useState(avatarImg);
+    const [imageUrl, setImageUrl] = useState('https://iili.io/JEijWG4.png');
     const warframUrl = 'https://www.wolframcloud.com/obj/407c7a68-d5d0-4a90-b115-e4d143b56338'
 
 
@@ -83,6 +85,16 @@ export default function SpeciesIdentifier() {
         identifyImage(imageUrl)
             .then(result => {
                 setResult(result)
+                toast.success(result, {
+                    position: "top-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
                 setPending(false)
             })
             .catch(error => console.error('Error:', error));
@@ -95,41 +107,56 @@ export default function SpeciesIdentifier() {
 
     };
     return (
-           <div className='background '>
-                <div>
-                <h1  className='pt-1 pb-3'>Animal <span style={{ color: '#B95F00 '}} > <b>Breed </b></span> Identification Program</h1>
-                    <input ref={inputRef} accept="image/*" capture="environment" className='d-none' type="file" onChange={handleImageChange} />
-                    <button ref={btnRef} className='d-none' onClick={handleUpload}>handleUpload</button>
-                    <div id="" className='three'>
-                        <div className=''>
-                            <img id='imageDisplay' className='' src={imageUrl} />
-                        </div>
-                        <div > 
-                            
-                            <button  className='bt btt0' onClick={triggerInputBrowse}>  Upload photo  </button>
-                        </div>
-                        {/* <h2><i>{pending.toString()}</i></h2> */}
-                    </div>
-
-
-                    {pending ? (
-                        <div className=''>
-                            <FontAwesomeIcon className='heartbeat' icon={fa.faPaw} />
-                        </div>
+        <div className='background tall'>
+            <div>
+                <h1 className='start'>Animal <span style={{ color: '#B95F00 ' }} > <b>Breed </b></span> Identification Program</h1>
+                <div className='other'>
+                    <h2 className='bold container mt-1 mb-3'>Upload a picture and press Identify to show the result!</h2>
+                    {Result == "Try it!" ? (
+                        <h3 className='mt-1 mb-3'>{Result}</h3>
                     ) : (
-                        <div>
-                           <br></br>
-                            <button  className='btt1' onClick={call}>Identify!</button>
-                        </div >
-                    )
-                    }
-                    
-                    <h3 > <p>{Result}</p> </h3>
+                        <h3 className='mt-1 mb-3 colorful'>{Result}</h3>
+                    )}
                 </div>
-                
+                <input ref={inputRef} accept="image/*" capture="environment" className='d-none' type="file" onChange={handleImageChange} />
+                <button ref={btnRef} className='d-none' onClick={handleUpload}>handleUpload</button>
+                <div id="" className='three'>
+                    <div className=''>
+                        {imageUrl == 'https://iili.io/JEP1YL7.png' ? (
+                            <img id='imageDisplay' className=' attention-grabber ' src={imageUrl} />
+                        ) : (
+                            <img id='imageDisplay' className=' ' src={imageUrl} />
+                        )}
+
+                    </div>
+                    <div className='mt-3'>
+                        <button className='btn btn-outline-orange' onClick={triggerInputBrowse}>  Upload photo  </button>
+                    </div>
+                    {/* <h2><i>{pending.toString()}</i></h2> */}
+                </div>
+
+
+                {pending ? (
+                    <div className='py-3'>
+                        <FontAwesomeIcon className='heartbeat' icon={fa.faPaw} />
+                    </div>
+                ) : (
+                    <div className='py-3'>
+                        <button className='btn btn-orange' onClick={call}>Identify!</button>
+                    </div >
+                )
+                }
+                <div className='phone'>
+                    <h2 className='bold container mt-1'>Upload a picture and press Identify to show the result!</h2>
+                    {Result == "Try it!" ? (
+                        <h3 className='mt-5'>{Result}</h3>
+                    ) : (
+                        <h3 className='mt-5 colorful'>{Result}</h3>
+                    )}
+                </div>
             </div>
-            
-      
+
+        </div>
     )
 
 }
