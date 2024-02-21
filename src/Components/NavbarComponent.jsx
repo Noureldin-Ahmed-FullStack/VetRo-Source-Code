f5bced05cf489b0e7ee2dba731b3f2a99916fb0a
Noureldin <noureldin2662002@gmail.com>
2024-02-21 16:33:30 +0200

    FIXED bot in nav

--- src/Components/NavbarComponent.jsx
+++ src/Components/NavbarComponent.jsx
@@ -46,44 +46,44 @@ export default function NavbarComponent() {
 
   }
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="navbar-brand ps-2 ms-5 d-flex align-items-center" >
+    <>
+      <VetRoBot />
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
 
-        <img src="/logo192.png" className='Nav-Image' alt="" />
-        <NavLink className="nav-link" to=""><h3>VetRo</h3></NavLink>
-      </div>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse justify-content-evenly " id="navbarNavAltMarkup">{/*bg-primary*/}
-        <div></div>
-        <ul className="navbar-nav ">
-          <li className="nav-item active">
-            <NavLink className="nav-link" to="">Home</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="speciesidentifier">speciesidentifier</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="room">Chat</NavLink>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" data-bs-toggle="offcanvas" data-bs-target="#demo1"><FontAwesomeIcon className='BigIcon navIcon px-2' icon={fa.faBars} /></Link>
-          </li>
-        </ul>
-        <div className='d-flex justify-content-center'>
-          <ul className="navbar-nav d-flex align-items-center">
-            <li className='text-center'>
-              <div className="position-relative">
-                {/* <FontAwesomeIcon className='cartIcon' icon={fa.faCartShopping} /> */}
-                <img src={UserDBData?.userPFP} className='Nav-PFP' alt="" />
-                {/* <span className="position-absolute top-0 start-80 translate-middle badge rounded-pill bg-danger">
+        <div className="navbar-brand ps-2 ms-5 d-flex align-items-center" >
+
+          <img src="/logo192.png" className='Nav-Image' alt="" />
+          <NavLink className="nav-link" to=""><h3>VetRo</h3></NavLink>
+        </div>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse justify-content-around " id="navbarNavAltMarkup">{/*bg-primary*/}
+          <div></div>
+          <ul className="navbar-nav ">
+            <li className="nav-item active">
+              <NavLink className="nav-link" to="">Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="speciesidentifier">speciesidentifier</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="room">Chat</NavLink>
+            </li>
+          </ul>
+          <div className='d-flex justify-content-center'>
+            <ul className="navbar-nav d-flex align-items-center">
+              <li className='text-center'>
+                <div className="position-relative">
+                  {/* <FontAwesomeIcon className='cartIcon' icon={fa.faCartShopping} /> */}
+                  <Link className="nav-link" data-bs-toggle="offcanvas" data-bs-target="#demo1"><img src={UserDBData?.userPFP} className='Nav-PFP' alt="" /></Link>
+                  {/* <span className="position-absolute top-0 start-80 translate-middle badge rounded-pill bg-danger">
                   {cart?.numOfCartItems? (<>{cart?.numOfCartItems}</>):(<>0</>)}
                                     <span className="visually-hidden">unread messages</span>
                 </span> */}
-              </div>
-            </li>
-            {/* {userObj ? (
+                </div>
+              </li>
+              {/* {userObj ? (
                             <li className='text-center mt-2 ms-4'>
                                 <button onClick={logOut} className="btn">log out</button>
                             </li>
@@ -98,10 +98,10 @@ export default function NavbarComponent() {
                                 </li>
                             </>
                         )} */}
-          </ul>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-
+      </nav>
+    </>
   )
 }
