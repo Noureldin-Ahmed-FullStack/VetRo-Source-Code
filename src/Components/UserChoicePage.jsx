b766ce47ac9b1357bcef247fd0081a77b3ab6a8b
Noureldin-Ahmed-FullStack <noureldin2662002@gmail.com>
2024-04-16 20:01:07 +0200

    fixed localhost

--- src/Components/UserChoicePage.jsx
+++ src/Components/UserChoicePage.jsx
@@ -45,7 +45,7 @@ export default function UserChoicePage() {
         const body ={
             isDoctor: Choice
         }
-            await axios.post('http://localhost:3000/userRole', body, { headers: headers })
+            await axios.post('https://vetro-server.onrender.com/userRole', body, { headers: headers })
             // setTimeout(() => {
                 window.location.reload();
             // }, 1000); 
